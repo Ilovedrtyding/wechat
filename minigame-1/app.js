ba80aec69f345f7968e78e98d0faafd7043e5686
Ilovedrtyding <45235412+Ilovedrtyding@users.noreply.github.com>
2023-12-10 19:29:50 +0800

    Add files via upload

---
+++ minigame-1/app.js
@@ -0,0 +1,30 @@
+// app.js
+App({
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if (res.code) {
+          // 获取用户信息
+          wx.getUserInfo({
+            success: userRes => {
+              this.globalData.userInfo = userRes.userInfo
+              // 可以将用户信息发送到后台保存，也可以在这里进行其他操作
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
